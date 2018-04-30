6e6370f5c33cf2b1c206b2a28412bd2331802385
Eyokiha <h.veldthuis@gmail.com>
2018-04-30 14:34:09 +0200

    Create users - scripts & data

--- create_users.py
+++ create_users.py
@@ -1,11 +1,16 @@
 import json
 import time
+from collections import Counter
+
+
+def divide(n, d):
+    return n / d if d else 0
 
 def newUser(data):
     return {
-     # "registered":  data["owner"]["user_type"]=="registered",    # registered or unregistered user
-     "accept_rate": data["owner"].get("accept_rate",None), # percentage of questions (of the user) with accepted answer
-     "reputation":  data["owner"]["reputation"],  # reputation of user
+     "registered":  data["owner"]["user_type"]=="registered",  # registered or unregistered user
+     "accept_rate": data["owner"].get("accept_rate",-1),     # percentage of questions (of the user) with accepted answer
+     "reputation":  data["owner"].get("reputation",-1),      # reputation of user
      "questions": {"total": 0,                    # total nr of questions of user
                    "code_blocks": 0,              # nr of questions that include code blocks
                    "tag_python": 0,               # nr of questions with tag python
@@ -31,83 +36,135 @@ def newUser(data):
                    "down_votes": [],              # list of downvotes per answer
                    "up_votes": [],                # list of up votes per answer
                    "accepted": 0,                 # nr of accepted answers
-                   "time":[]                      # list of times in s between question and answer
+                   "time": [],                    # list of times in s between question and answer
+                   "questioners": []              # list of user id's of whom the user answered a question
                   }
     }
+
+
+def fillUser(item, userdict):
+    # Only get data with correct tag(s)
+    if "c" in item["tags"] or "c++" in item["tags"] or "java" in item["tags"] or "javascript" in item["tags"] or "python" in item["tags"]:
+        # date = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(item["creation_date"]))
+        # print(item)
+        user_id = item["owner"].get("user_id", item["owner"]["display_name"])
+        # if item["owner"]["user_type"] == "registered":
+        if not user_id in userdict:
+            userdict[user_id] = newUser(item)
+        
+        userdict[user_id]["questions"]["total"]           += 1
+        userdict[user_id]["questions"]["code_blocks"]     += int('<pre><code>' in item["body"])
+        userdict[user_id]["questions"]["tag_python"]      += int("python"      in item["tags"])
+        userdict[user_id]["questions"]["tag_javascript"]  += int("javascript"  in item["tags"])
+        userdict[user_id]["questions"]["tag_java"]        += int("java"        in item["tags"])
+        userdict[user_id]["questions"]["tag_c++"]         += int("c++"         in item["tags"])
+        userdict[user_id]["questions"]["tag_c"]           += int("c"           in item["tags"])
+        userdict[user_id]["questions"]["scores"].append(item["score"])
+        userdict[user_id]["questions"]["down_votes"].append(item["down_vote_count"])
+        userdict[user_id]["questions"]["up_votes"].append(item["up_vote_count"])
+        userdict[user_id]["questions"]["is_answered"]     += int(item["is_answered"])
+        userdict[user_id]["questions"]["answer_count"].append(item["answer_count"])
+        userdict[user_id]["questions"]["view_count"].append(item["view_count"])
+
+        questioner = user_id
+
+        for answer in item["answers"]:
+            # if answer["owner"]["user_type"] == "registered":
+            user_id = answer["owner"].get("user_id", answer["owner"]["display_name"])
+            if not user_id in userdict:
+                userdict[user_id] = newUser(answer)
+            
+            userdict[user_id]["answers"]["total"]           += 1
+            userdict[user_id]["answers"]["code_blocks"]     += int('<pre><code>' in answer["body"])
+            userdict[user_id]["answers"]["tag_python"]      += int("python"      in item["tags"])
+            userdict[user_id]["answers"]["tag_javascript"]  += int("javascript"  in item["tags"])
+            userdict[user_id]["answers"]["tag_java"]        += int("java"        in item["tags"])
+            userdict[user_id]["answers"]["tag_c++"]         += int("c++"         in item["tags"])
+            userdict[user_id]["answers"]["tag_c"]           += int("c"           in item["tags"])
+            userdict[user_id]["answers"]["scores"].append(answer["score"])
+            userdict[user_id]["answers"]["down_votes"].append(answer["down_vote_count"])
+            userdict[user_id]["answers"]["up_votes"].append(answer["up_vote_count"])
+            userdict[user_id]["answers"]["accepted"]        += int(answer["is_accepted"])
+            userdict[user_id]["answers"]["time"].append(item["creation_date"] - answer["creation_date"])
+            userdict[user_id]["answers"]["questioners"].append(questioner)
+
+    return userdict
+
+
+def avgUser(userdict):
+    for user_id in userdict:
+        # Averaging scores
+        userdict[user_id]["questions"]["scores"]       = divide(sum(userdict[user_id]["questions"]["scores"])       , len(userdict[user_id]["questions"]["scores"]))
+        userdict[user_id]["questions"]["down_votes"]   = divide(sum(userdict[user_id]["questions"]["down_votes"])   , len(userdict[user_id]["questions"]["down_votes"]))
+        userdict[user_id]["questions"]["up_votes"]     = divide(sum(userdict[user_id]["questions"]["up_votes"])     , len(userdict[user_id]["questions"]["up_votes"]))
+        userdict[user_id]["questions"]["answer_count"] = divide(sum(userdict[user_id]["questions"]["answer_count"]) , len(userdict[user_id]["questions"]["answer_count"]))
+        userdict[user_id]["questions"]["view_count"]   = divide(sum(userdict[user_id]["questions"]["view_count"])   , len(userdict[user_id]["questions"]["view_count"]))
+        
+        userdict[user_id]["answers"]["scores"]     = divide(sum(userdict[user_id]["answers"]["scores"])     , len(userdict[user_id]["answers"]["scores"]))
+        userdict[user_id]["answers"]["down_votes"] = divide(sum(userdict[user_id]["answers"]["down_votes"]) , len(userdict[user_id]["answers"]["down_votes"]))
+        userdict[user_id]["answers"]["up_votes"]   = divide(sum(userdict[user_id]["answers"]["up_votes"])   , len(userdict[user_id]["answers"]["up_votes"]))
+        userdict[user_id]["answers"]["time"]       = divide(sum(userdict[user_id]["answers"]["time"])       , len(userdict[user_id]["answers"]["time"]))
+        
+        userdict[user_id]["answers"]["questioners"] = dict(Counter(userdict[user_id]["answers"]["questioners"]))
+
+    return userdict
+
 
 def main():
     with open('so_questions_answers_2017.txt', 'r') as rf:
         with open('dataset_users.txt','w') as wf:
-            userdict = {}
-
-            # userdictJan = {}
-            # userdictFeb = {}
-            # userdictMar = {}
-            # userdictApr = {}
-            # userdictMay = {}
+            userdict    = {}
+            userdictJan = {}
+            userdictFeb = {}
+            userdictMar = {}
+            userdictApr = {}
+            userdictMay = {}
+            userdictJun = {}
 
             for line in rf:
-                item = json.loads(line)
+                item  = json.loads(line)
+                userdict = fillUser(item, userdict)
 
-                # Only get data with correct tag(s)
-                if "c" in item["tags"] or "c++" in item["tags"] or "java" in item["tags"] or "javascript" in item["tags"] or "python" in item["tags"]:
-                    # date = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(item["creation_date"]))
-                    # print(item)
-                    if item["owner"]["user_type"] == "registered":
-	                    user_id = item["owner"]["user_id"]
-	                    if not user_id in userdict:
-	                        userdict[user_id] = newUser(item)
-	                    
-	                    userdict[user_id]["questions"]["total"]           += 1
-	                    userdict[user_id]["questions"]["code_blocks"]     += int('<pre><code>' in item["body"])
-	                    userdict[user_id]["questions"]["tag_python"]      += int("python"      in item["tags"])
-	                    userdict[user_id]["questions"]["tag_javascript"]  += int("javascript"  in item["tags"])
-	                    userdict[user_id]["questions"]["tag_java"]        += int("java"        in item["tags"])
-	                    userdict[user_id]["questions"]["tag_c++"]         += int("c++"         in item["tags"])
-	                    userdict[user_id]["questions"]["tag_c"]           += int("c"           in item["tags"])
-	                    userdict[user_id]["questions"]["scores"].append(item["score"])
-	                    userdict[user_id]["questions"]["down_votes"].append(item["down_vote_count"])
-	                    userdict[user_id]["questions"]["up_votes"].append(item["up_vote_count"])
-	                    userdict[user_id]["questions"]["is_answered"]     += int(item["is_answered"])
-	                    userdict[user_id]["questions"]["answer_count"].append(item["answer_count"])
-	                    userdict[user_id]["questions"]["view_count"].append(item["view_count"])
-
-
-                    for answer in item["answers"]:
-                    	if answer["owner"]["user_type"] == "registered":
-	                        user_id = answer["owner"]["user_id"]
-	                        if not user_id in userdict:
-	                            userdict[user_id] = newUser(answer)
-	                        
-	                        userdict[user_id]["answers"]["total"]           += 1
-	                        userdict[user_id]["answers"]["code_blocks"]     += int('<pre><code>' in answer["body"])
-	                        userdict[user_id]["answers"]["tag_python"]      += int("python"      in item["tags"])
-	                        userdict[user_id]["answers"]["tag_javascript"]  += int("javascript"  in item["tags"])
-	                        userdict[user_id]["answers"]["tag_java"]        += int("java"        in item["tags"])
-	                        userdict[user_id]["answers"]["tag_c++"]         += int("c++"         in item["tags"])
-	                        userdict[user_id]["answers"]["tag_c"]           += int("c"           in item["tags"])
-	                        userdict[user_id]["answers"]["scores"].append(answer["score"])
-	                        userdict[user_id]["answers"]["down_votes"].append(answer["down_vote_count"])
-	                        userdict[user_id]["answers"]["up_votes"].append(answer["up_vote_count"])
-	                        userdict[user_id]["answers"]["accepted"]        += int(answer["is_accepted"])
-	                        userdict[user_id]["answers"]["time"].append(item["creation_date"] - answer["creation_date"])
-
-                    
-                    
-            userdata = json.dumps(userdict) # controleren of het een enkele regel is, geen newlines
-            wf.write(userdata) # schrijf naar file
-                    
-                    
-
-
-
-
-
+                month = time.strftime("%b", time.localtime(item["creation_date"]))
+                if month == "Jan":
+                    userdictJan = fillUser(item, userdictJan)
+                if month == "Feb":
+                    userdictFeb = fillUser(item, userdictFeb)
+                if month == "Mar":
+                    userdictMar = fillUser(item, userdictMar)
+                if month == "Apr":
+                    userdictApr = fillUser(item, userdictApr)
+                if month == "May":
+                    userdictMay = fillUser(item, userdictMay)
+                if month == "Jun":
+                    userdictJun = fillUser(item, userdictJun)
                 
+            # userdata = json.dumps(userdict)
+            # wf.write(userdata)
 
 
+    # with open('dataset_users.txt','r') as rf:
+    #     with open('dataset_users_avgd.txt','w') as wf:
+    #         line = rf.readline()
+    #         userdict = json.loads(line)
 
+            userdict    = avgUser(userdict)
+            userdictJan = avgUser(userdictJan)
+            userdictFeb = avgUser(userdictFeb)
+            userdictMar = avgUser(userdictMar)
+            userdictApr = avgUser(userdictApr)
+            userdictMay = avgUser(userdictMay)
+            userdictJun = avgUser(userdictJun)
+            
+            userdata    = json.dumps(userdict)
+            userdataJan = json.dumps(userdictJan)
+            userdataFeb = json.dumps(userdictFeb)
+            userdataMar = json.dumps(userdictMar)
+            userdataApr = json.dumps(userdictApr)
+            userdataMay = json.dumps(userdictMay)
+            userdataJun = json.dumps(userdictJun)
 
+            wf.write(userdata)
 
 
 if __name__ == '__main__':
---
+++ create_users_csv.py
@@ -0,0 +1,84 @@
+import json
+import csv
+
+def main():
+    with open('dataset_users.txt', 'r') as rf:
+        with open('dataset_users_nodes.csv','w') as wfn:
+            with open('dataset_users_edges.csv','w') as wfe:
+                line = rf.readline()
+                userdict = json.loads(line)
+
+                wn = csv.writer(wfn)
+                we = csv.writer(wfe)
+
+                wn.writerow(["reputation"
+                            ,"accept_rate"
+                            ,"Q:total"
+                            ,"Q:code_blocks"
+                            ,"Q:tag_python"
+                            ,"Q:tag_javascript"
+                            ,"Q:tag_java"
+                            ,"Q:tag_c++"
+                            ,"Q:tag_c"
+                            ,"Q:scores"
+                            ,"Q:down_votes"
+                            ,"Q:up_votes"
+                            ,"Q:is_answered"
+                            ,"Q:answer_count"
+                            ,"Q:view_count"
+                            ,"A:total"
+                            ,"A:code_blocks"
+                            ,"A:tag_python"
+                            ,"A:tag_javascript"
+                            ,"A:tag_java"
+                            ,"A:tag_c++"
+                            ,"A:tag_c"
+                            ,"A:scores"
+                            ,"A:down_votes"
+                            ,"A:up_votes"
+                            ,"A:accepted"
+                            ,"A:time"])
+                we.writerow(["answerer","questioner"])
+
+                for user in userdict:
+                    wn.writerow([user
+                                ,userdict[user]["questions"]["reputation"]
+                                ,userdict[user]["questions"]["accept_rate"]
+                                ,userdict[user]["questions"]["total"]
+                                ,userdict[user]["questions"]["code_blocks"]
+                                ,userdict[user]["questions"]["tag_python"]
+                                ,userdict[user]["questions"]["tag_javascript"]
+                                ,userdict[user]["questions"]["tag_java"]
+                                ,userdict[user]["questions"]["tag_c++"]
+                                ,userdict[user]["questions"]["tag_c"]
+                                ,userdict[user]["questions"]["scores"]
+                                ,userdict[user]["questions"]["down_votes"]
+                                ,userdict[user]["questions"]["up_votes"]
+                                ,userdict[user]["questions"]["is_answered"]
+                                ,userdict[user]["questions"]["answer_count"]
+                                ,userdict[user]["questions"]["view_count"]
+                                ,userdict[user]["answers"]["total"]
+                                ,userdict[user]["answers"]["code_blocks"]
+                                ,userdict[user]["answers"]["tag_python"]
+                                ,userdict[user]["answers"]["tag_javascript"]
+                                ,userdict[user]["answers"]["tag_java"]
+                                ,userdict[user]["answers"]["tag_c++"]
+                                ,userdict[user]["answers"]["tag_c"]
+                                ,userdict[user]["answers"]["scores"]
+                                ,userdict[user]["answers"]["down_votes"]
+                                ,userdict[user]["answers"]["up_votes"]
+                                ,userdict[user]["answers"]["accepted"]
+                                ,userdict[user]["answers"]["time"]])
+                    for q in userdict[user]["answers"]["questioners"]:
+                        we.writerow([user,q])
+                    
+
+                # print(item)
+                # w.writerow(item.values())
+
+
+
+
+if __name__ == '__main__':
+    main()
+
